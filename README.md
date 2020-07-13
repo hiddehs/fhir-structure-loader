fdc46538360c4b1e5ccedaf2e32f39e50ccac57b
hiddehs <hidde.schultze@gmail.com>
2020-07-13 12:32:44 +0200

    Initial 🔥 FHIR StructureDefintion Loader commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # fhir-structure-loader
-Adminstration endpoint FHIR StructureDefintion loader from Simplifier packages
+Adminstration endpoint FHIR StructureDefintion `shellscript` loader from Simplifier packages.
+
+## Requirements
+
+- `curl` installed (Mac OSX: `brew install curl`)
+- `jq` [installed](https://stedolan.github.io/jq/download/) (Mac OSX: `brew install jq`)
+
+## Getting started
+
+```bash
+# clone repo & make structureloader executable
+git clone https://github.com/hiddehs/fhir-structure-loader.git
+chmod +x structureloader.sh
+```
+
+Download your Resources JSON zip from Simplifier and unpack it into the package directory.
+
+## Usage
+```bash
+./structureloader --url={administration_endpoint_url}
+```
+
+## Arguments
+
+| Argument | Value |
+| -------- | ----- |
+| `--url -u`   | `/Administration` endpoint url (default: `http://localhost:8080/Administration`) |
+| `--package -p`    | package directory that contains subdirectory with Simplifier Package extracted (default: `package`) |
---
+++ structureloader.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+endpoint="http://localhost:8080/Administration"
+packageDirectory="package"
+definition_count=0
+
+function loadStructureDefintions() {
+  echo "Loading StructureDefinitions in $1"
+  for jsonFile in $1*.json; do
+    if [ "$(cat $jsonFile | ./jq-osx-amd64 '.resourceType')" = '"StructureDefinition"' ]; then
+      definition_count=definition_count+1
+      id=$(cat $jsonFile | ./jq-osx-amd64 '.id' | sed 's/"//g')
+      cmd="curl -sL -H \"Content-Type: application/json\" -X PUT --data  \"@$jsonFile\" -w '%{http_code}' $endpoint/StructureDefinition/$id -o /dev/null"
+      statusCode=$(eval "$cmd")
+      if [ $statusCode = "201" ] | [ $statusCode = "200" ]; then
+        echo "[$statusCode] Loaded $jsonFile to $endpoint/StructureDefinition/$id "
+      else
+        echo "[$statusCode] Failed to load $jsonFile to $endpoint/StructureDefinition/$id"
+      fi
+    fi
+  done
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --url* | -u*)
+    if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+    URL="${1#*=}"
+    ;;
+  --package* | -p*)
+    if [[ "$1" != *=* ]]; then shift; fi
+    PACKAGE="${1#*=}"
+    ;;
+  --help | -h)
+    printf "Usage: ./structureloader.sh [--url={Administration Endpoint} --package={Directory that contains unzipped package directory}]" # Flag argument
+    exit 0
+    ;;
+  *)
+    printf >&2 "Error: Invalid argument\n"
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+
+if [ -n "$URL" ]; then endpoint=$URL; fi
+if [ -n "$PACKAGE" ]; then packageDirectory=$PACKAGE; fi
+
+echo "Configured Administration endpoint: $endpoint"
+echo "Configured Package directory: $packageDirectory"
+
+for d in $packageDirectory/*/; do
+  loadStructureDefintions $d
+  return
+done
+
+echo "Successfully Loaded $definition_count StructureDefinitions"
